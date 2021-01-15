033b3340b17ff48bb0a96b3d76fbecf36f64780d
Or Pinhas <38883550+orpinhas20@users.noreply.github.com>
2021-01-15 19:43:33 +0200

    Map (#1)
    * feat: adding map_api
    * fix: adding permissions to manifest
    * fix:  adding  new functions to contactFragment

--- MyNutrition/app/src/main/java/com/example/mynutrition/MainActivity.java
+++ MyNutrition/app/src/main/java/com/example/mynutrition/MainActivity.java
@@ -1,53 +1,64 @@
 package com.example.mynutrition;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-
 import com.example.mynutrition.ui.daily_status.DailyStatusFragment;
 import com.example.mynutrition.ui.diet_dictionary.DietDictionaryFragment;
 import com.example.mynutrition.ui.food_tracking.FoodTrackingFragment;
 import com.example.mynutrition.ui.contact.ContactFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.Manifest;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
-    DailyStatusFragment dailyStatusFragment;
-    DietDictionaryFragment dietDictionaryFragment;
-    FoodTrackingFragment foodTrackingFragment;
-    ContactFragment contactFragment;
-    Fragment activeFragment;
+    private DailyStatusFragment dailyStatusFragment;
+    private DietDictionaryFragment dietDictionaryFragment;
+    private FoodTrackingFragment foodTrackingFragment;
+    private ContactFragment contactFragment;
+    private Fragment activeFragment;
+    private FragmentManager fragmentManager;
+    private BottomNavigationView navView;
     private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         initBottomBar();
+
     }
 
     private void initBottomBar() {
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        final FragmentManager fragmentManager = getSupportFragmentManager();
+        navView = findViewById(R.id.nav_view);
+        fragmentManager = getSupportFragmentManager();
         dietDictionaryFragment = new DietDictionaryFragment();
         dailyStatusFragment = new DailyStatusFragment();
         foodTrackingFragment = new FoodTrackingFragment();
         contactFragment = new ContactFragment();
         activeFragment = dietDictionaryFragment;
-        updateTile(R.id.navigation_daily_status);
+        updateTitle(R.id.navigation_daily_status);
 
-        fragmentManager.beginTransaction().add(R.id.nav_host_fragment, contactFragment, "3").hide(contactFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.nav_host_fragment, contactFragment, "4").hide(contactFragment).commit();
         fragmentManager.beginTransaction().add(R.id.nav_host_fragment, foodTrackingFragment, "3").hide(foodTrackingFragment).commit();
         fragmentManager.beginTransaction().add(R.id.nav_host_fragment, dietDictionaryFragment, "2").hide(dailyStatusFragment).commit();
         fragmentManager.beginTransaction().add(R.id.nav_host_fragment, dailyStatusFragment, "1").commit();
@@ -59,22 +70,22 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.navigation_daily_status:
                         fragmentManager.beginTransaction().hide(activeFragment).show(dailyStatusFragment).commit();
                         activeFragment = dailyStatusFragment;
-                        updateTile(item.getItemId());
+                        updateTitle(item.getItemId());
                         return true;
                     case R.id.navigation_diet_dictionary:
                         fragmentManager.beginTransaction().hide(activeFragment).show(dietDictionaryFragment).commit();
                         activeFragment = dietDictionaryFragment;
-                        updateTile(item.getItemId());
+                        updateTitle(item.getItemId());
                         return true;
                     case R.id.navigation_food_tracking:
                         fragmentManager.beginTransaction().hide(activeFragment).show(foodTrackingFragment).commit();
                         activeFragment = foodTrackingFragment;
-                        updateTile(item.getItemId());
+                        updateTitle(item.getItemId());
                         return true;
                     case R.id.navigation_contact:
                         fragmentManager.beginTransaction().hide(activeFragment).show(contactFragment).commit();
                         activeFragment = contactFragment;
-                        updateTile(item.getItemId());
+                        updateTitle(item.getItemId());
                         return true;
                 }
                 return false;
@@ -82,13 +93,15 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void updateTile(int id) {
+    private void updateTitle(int id) {
         if (id == R.id.navigation_daily_status) {
             getSupportActionBar().setTitle(R.string.title_daily_status);
         } else if (id == R.id.navigation_diet_dictionary) {
             getSupportActionBar().setTitle(R.string.title_diet_dictionary);
         } else if (id == R.id.navigation_food_tracking) {
             getSupportActionBar().setTitle(R.string.title_food_tracking);
+        } else if (id == R.id.navigation_contact) {
+            getSupportActionBar().setTitle(R.string.title_contact);
         }
     }
 
